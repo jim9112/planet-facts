ea3abca733e4c51cdd28c22b5357d2e0da95543e
jim9112 <jim9112@gmail.com>
2021-07-06 16:43:19 -0400

    add mode nav

--- components/PlanetContainer.tsx
+++ components/PlanetContainer.tsx
@@ -15,23 +15,32 @@ type CompProps = {
 
 const PlanetContainer = ({ planet }: CompProps) => {
   return (
-    <div>
-      <Image
-        width="200px"
-        height="200px"
-        src={planet.images.planet}
-        alt={planet.name}
-      />
+    <div className="grid grid-cols-2">
+      <div>
+        <Image
+          width="200px"
+          height="200px"
+          src={planet.images.planet}
+          alt={planet.name}
+        />
+      </div>
       <div>
         <h1>{planet.name}</h1>
         <p>{planet.overview.content}</p>
+        <span>
+          Source:{' '}
+          <a href={planet.overview.source} target="blank">
+            Wikapedia
+          </a>
+        </span>
+        <nav>
+          <ul>
+            <li>01 OVERVIEW</li>
+            <li>02 INTERNAL STRUCTURE</li>
+            <li>03 SURFACE GEOLOGY</li>
+          </ul>
+        </nav>
       </div>
-      <span>
-        Source:{' '}
-        <a href={planet.overview.source} target="blank">
-          Wikapedia
-        </a>
-      </span>
     </div>
   );
 };
