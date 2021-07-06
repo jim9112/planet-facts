ccea6c5d7a1d29ac4641d902710ec5bffe3f383a
jim9112 <jim9112@gmail.com>
2021-07-06 14:41:01 -0400

    add logic for switching through the planets

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,20 @@
-export default function Header() {
+export default function Header({ findPlanet }: any) {
+  // changes planet on the main screen
+  const handleClick = (planetName: string) => {
+    findPlanet(planetName);
+  };
   return (
     <div className="grid grid-flow-col">
       <h1>Planets</h1>
       <ul className="grid grid-flow-col">
-        <li>MERCURY</li>
-        <li>VENUS</li>
-        <li>EARTH</li>
-        <li>MARS</li>
-        <li>JUPITER</li>
-        <li>SATURN</li>
-        <li>URANUS</li>
-        <li>NEPTUNE</li>
+        <li onClick={() => handleClick('Mercury')}>MERCURY</li>
+        <li onClick={() => handleClick('Venus')}>VENUS</li>
+        <li onClick={() => handleClick('Earth')}>EARTH</li>
+        <li onClick={() => handleClick('Mars')}>MARS</li>
+        <li onClick={() => handleClick('Jupiter')}>JUPITER</li>
+        <li onClick={() => handleClick('Saturn')}>SATURN</li>
+        <li onClick={() => handleClick('Uranus')}>URANUS</li>
+        <li onClick={() => handleClick('Neptune')}>NEPTUNE</li>
       </ul>
     </div>
   );
