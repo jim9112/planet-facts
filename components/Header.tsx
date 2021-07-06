2cdef82e69a54d240d5162dc6b7d48cd90d4a3fe
jim9112 <jim9112@gmail.com>
2021-07-06 13:02:19 -0400

    add header component

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <div className="grid grid-flow-col">
+      <h1>Planets</h1>
+      <ul className="grid grid-flow-col">
+        <li>MERCURY</li>
+        <li>VENUS</li>
+        <li>EARTH</li>
+        <li>MARS</li>
+        <li>JUPITER</li>
+        <li>SATURN</li>
+        <li>URANUS</li>
+        <li>NEPTUNE</li>
+      </ul>
+    </div>
+  );
+}
