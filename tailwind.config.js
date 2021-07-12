ad22f536fb1f7d5b5f023bc733f45e3d4f532992
jim9112 <jim9112@gmail.com>
2021-07-12 15:56:57 -0400

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './pages/**/*.js',
+    './pages/**/*.ts',
+    './pages/**/*.jsx',
+    './pages/**/*.tsx',
+    './components/**/*.js',
+    './components/**/*.ts',
+    './components/**/*.jsx',
+    './components/**/*.tsx',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
